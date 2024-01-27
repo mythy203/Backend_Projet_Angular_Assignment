e3d7a9d7db7cf41ed0a30437ccd162c909788554
mythy203 <thuytrinh.hnue@gmail.com>
2024-01-27 19:05:58 +0100

    clean code

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,10 @@ const saltRounds = 10;
 const { User } = require('../model/user');
 
 router.get('/', (req, res) => {
-    // Thêm logic xác thực nếu cần
     User.find({}, (err, users) => {
         if (err) {
             return res.status(500).send('Error on the server.');
         }
-        // Trả về thông tin người dùng
         res.status(200).send(users);
     });
 });
@@ -28,19 +26,16 @@ router.post('/login', (req, res) => {
         
         bcrypt.compare(req.body.password, user.password, function(err, result) {
             if (result === true) {
-                // Tạo token JWT chứa id, username và role của người dùng
                 const token = jwt.sign(
                     { id: user._id, username: user.username, role: user.role },
-                    'SECRET_KEY', // Nên sử dụng biến môi trường cho SECRET_KEY
+                    'SECRET_KEY', 
                     { expiresIn: '24h' }
                 );
-                // Trả về token và thông tin người dùng
                 res.json({
                     auth: true,
                     token: token,
                     username:user.username,
-                    role: user.role // Trả về role của người dùng
-                    // Bạn có thể thêm bất kỳ thông tin người dùng nào khác bạn muốn gửi
+                    role: user.role 
                 });
             } else {
                 return res.status(401).send({ auth: false, token: null });
@@ -49,8 +44,6 @@ router.post('/login', (req, res) => {
     });
 });
 router.get('/login', (req, res) => {
-    // Kiểm tra trạng thái đăng nhập của người dùng ở đây
-    // Ví dụ: nếu đã đăng nhập, trả về thông tin người dùng, ngược lại trả về thông báo
     if (req.isAuthenticated()) {
         res.json({
             auth: true,
